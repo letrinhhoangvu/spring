87d3aeebf0503704d9898b8de632907ae6795b08
vulth <v2computes2004@yahoo.com>
2015-12-21 15:02:00 +0700

    hello spring

---
+++ src/vu/hellospring/HelloWorld.java
@@ -0,0 +1,11 @@
+package vu.hellospring;
+
+public class HelloWorld {
+	private String message;
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public void getMessage() {
+		System.out.println("Your message :" + message);
+	}
+}
